e4a0456b7c153f16ef83650e1925fa71f7800600
Elena Tereshina <vpelena110981@gmail.com>
2020-08-12 16:20:26 +0300

    final update

---
+++ src/data/dataDoctors.js
@@ -0,0 +1,88 @@
+const doctors = [
+  {
+    id: 'd001',
+    firstName: 'Мария',
+    middleName: 'Петровна',
+    surname: 'Иванова',
+    speciality: 's001',
+    gender: 'female',
+    experience: '15 лет',
+    priceCoeff: 1.3,
+    picture: '', //dynamic 'images/'+ '001' + 'small' or 'big' + 'jpg'
+    details:
+      'Более 15 лет трудилась на нелегом поприще терапии. Заслуженный врач Москвы. Специализируется на терапии головы и предголовья',
+  },
+  {
+    id: 'd002',
+    firstName: 'Наталья',
+    middleName: 'Ивановна',
+    surname: 'Кабанова',
+    speciality: 's001',
+    gender: 'female',
+    experience: '7 лет',
+    priceCoeff: 1,
+    picture: '', //dynamic 'images/'+ '001' + 'small' or 'big' + 'jpg'
+    details:
+      'Более 7 лет трудилась на нелегом поприще терапии. Заслуженный врач Щелково. Специализируется на терапии ',
+  },
+  {
+    id: 'd003',
+    firstName: 'Иван',
+    middleName: 'Николаевич',
+    surname: 'Мясников',
+    speciality: 's002',
+    gender: 'male',
+    experience: '8 лет',
+    priceCoeff: 1.2,
+    picture: '', //dynamic 'images/'+ '001' + 'small' or 'big' + 'jpg'
+    details:
+      'Более 8 лет трудился на нелегом поприще хирургии. Специализируется на удалении прыщей',
+  },
+  {
+    id: 'd004',
+    firstName: 'Бирджабек',
+    middleName: 'Галигбекович',
+    surname: 'Смирнов',
+    speciality: 's003',
+    gender: 'male',
+    experience: '10 лет',
+  },
+  {
+    id: 'd005',
+    firstName: 'Капитолина',
+    middleName: 'Эдуардовна',
+    surname: 'Хрустикова',
+    speciality: 's004',
+    gender: 'female',
+    experience: '12 лет',
+  },
+  {
+    id: 'd006',
+    firstName: 'Петр',
+    middleName: 'Григорьевич',
+    surname: 'Александров',
+    speciality: 's005',
+    gender: 'male',
+    experience: '11 лет',
+  },
+  {
+    id: 'd007',
+    firstName: 'Анастас',
+    middleName: 'Аппалонович',
+    surname: 'Высокосмотрящий',
+    speciality: 's006',
+    gender: 'male',
+    experience: '12 лет',
+  },
+  {
+    id: 'd008',
+    firstName: 'Анна',
+    middleName: 'Ивановна',
+    surname: 'Справкина',
+    speciality: 's009',
+    gender: 'female',
+    experience: '25 лет',
+  },
+];
+
+export default doctors;
