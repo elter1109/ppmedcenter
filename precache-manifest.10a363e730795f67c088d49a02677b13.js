b29edfd4b37d8a39fe2f2bd216b4065f42a9ea7d
Elena Tereshina <vpelena110981@gmail.com>
2020-08-12 16:33:17 +0300

    Updates

---
+++ precache-manifest.10a363e730795f67c088d49a02677b13.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be3b5d806e44a6c1b64ee3977391a87",
+    "url": "/ppmedcenter/index.html"
+  },
+  {
+    "revision": "7999cc7e4dc4a1ef5b81",
+    "url": "/ppmedcenter/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "46214392d03cb5448065",
+    "url": "/ppmedcenter/static/css/main.08f92856.chunk.css"
+  },
+  {
+    "revision": "7999cc7e4dc4a1ef5b81",
+    "url": "/ppmedcenter/static/js/2.c04ff3d4.chunk.js"
+  },
+  {
+    "revision": "46214392d03cb5448065",
+    "url": "/ppmedcenter/static/js/main.8ad29d7f.chunk.js"
+  },
+  {
+    "revision": "b38fce13f90cea0d0a1c",
+    "url": "/ppmedcenter/static/js/runtime~main.7d34283f.js"
+  },
+  {
+    "revision": "a6b658636d77271aec8de538fc64fb0c",
+    "url": "/ppmedcenter/static/media/docFemale.a6b65863.jpg"
+  },
+  {
+    "revision": "269f45593891430923bd0c80e36e1834",
+    "url": "/ppmedcenter/static/media/docMale.269f4559.jpg"
+  },
+  {
+    "revision": "2f22e7c7479da757bb05a0dbba9a6b2e",
+    "url": "/ppmedcenter/static/media/imageBlank.2f22e7c7.svg"
+  },
+  {
+    "revision": "02421cbadcd246f589d6f3dd3b736692",
+    "url": "/ppmedcenter/static/media/img.02421cba.jpg"
+  }
+]);
